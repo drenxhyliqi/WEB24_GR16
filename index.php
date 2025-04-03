f90584632dd5a1ceb3463b35c32f16919a80be54
drenxhyliqi <113259185+drenxhyliqi@users.noreply.github.com>
2025-04-03 13:14:56 +0200

    Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Car Marketplace | Home</title>
+    <title>Car Marketplace | Hommemmee</title>
     <link rel="icon" href="assets/img/webicon.png" type="image/x-icon">
 
     <!-- Bootstrap & Icons -->
