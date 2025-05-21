f44d6e9f670befde49aeae6b5e983f25d338111c
dajvazi <donart.ajvazi@hotmail.com>
2025-05-21 14:13:42 +0200

    creating db_connection

---
+++ db_conn.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "carme";
+
+    $con = mysqli_connect($servername , $username, $password , $dbname);
+
+    if(!$con){
+        echo "Db connection error..." . mysqli_connect_error();
+    }      
+?>
