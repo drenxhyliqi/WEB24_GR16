9108f84dc631bf4f412c422be99cb678341db49f
drenxhyliqi <drenxhyliqi3@gmail.com>
2025-05-25 23:16:57 +0200

    Small merge conflict fix

--- myProfile.php
+++ myProfile.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 if (isset($_POST['logout'])) {
@@ -75,31 +76,27 @@ $profileImage = 'assets/profile_default.png';
                 <ul class="navbar-nav mx-auto d-flex align-items-left gap-1 py-2">
                     <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="products.php">Cars</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="cars.php">Cars</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
                 </ul>
                 <ul class="navbar-nav d-flex align-items-right flex-row py-1">
-                    <li class="nav-item">
-                        <a class="nav-link" href="cars.php"><i class="bi bi-car-front-fill fs-4"></i></a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="cars.php"><i class="bi bi-cash-coin fs-4"></i></a>
-                    </li>
-
                     <?php if (isset($_SESSION['active'])): ?>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="bi bi-person-circle fs-4"></i>
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <?php  if ($_SESSION['user_role'] == 'admin'): ?>
-                                    <li><button class="dropdown-item" href="admin/dashboard.php">Dashboard</button></li>
+                            <ul class="dropdown-menu " aria-labelledby="dropdownMenuLink">
+                                <?php if ($_SESSION['user_role'] == 'admin'): ?>
+                                    <li><a class="dropdown-item navList" href="admin/dashboard.php">Dashboard</a></li>
                                 <?php endif; ?>
-                                <li><button class="dropdown-item" href="myProfile.php">My Profile</button></li>
-                                <form method="post" onsubmit="return confirm('A jeni i sigurt që doni të dilni?');">
-                                    <button type="submit" name="logout" class="dropdown-item">Logout</button>
-                                </form>
+
+                                <li><a class="dropdown-item navList" href="#">My Profile</a></li>
+
+                                <li>
+                                    <form method="post" style="margin: 0;" onsubmit="return confirm('A jeni i sigurt që doni të dilni?');">
+                                        <button type="submit" name="logout" class="logoutBtn ">Logout</button>
+                                    </form>
+                                </li>
                             </ul>
                         </li>
                     <?php else: ?>
@@ -108,6 +105,7 @@ $profileImage = 'assets/profile_default.png';
                         </li>
                     <?php endif; ?>
                     </li>
+                     <li class="nav-item"><a class="nav-link" href="favorite.php"><i class="bi bi-bag-heart fs-4"></i></a></li>
                 </ul>
             </div>
         </div>
